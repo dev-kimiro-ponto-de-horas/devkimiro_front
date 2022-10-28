9eb84ab0efe4c90d10b917f9cbffc99e8cec22a9
torh <torh@gft.com>
2022-10-28 11:09:27 -0300

    Arrumando o path de rota

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -3,14 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  {
-    path: "",
-    redirectTo: "login"
-  },
-  {
-    path: "login",
-    component: LoginComponent
-  }
+  {path: '', pathMatch: 'full' , redirectTo: 'login'},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
