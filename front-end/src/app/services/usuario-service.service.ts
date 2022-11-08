f4acf2369fb3dcabdc26f7d66bd1b0b11c620e33
torh <torh@gft.com>
2022-11-08 17:06:25 -0300

    corrindo o nome da service para futuros problemas

--- front-end/src/app/services/usuario-service.service.ts
+++ front-end/src/app/services/usuario-service.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { UsuarioSistema } from '../classes/usuarioSistema';
+import { UsuarioSistema } from '../classes/UsuarioSistema';
 
 @Injectable({
   providedIn: 'root'
