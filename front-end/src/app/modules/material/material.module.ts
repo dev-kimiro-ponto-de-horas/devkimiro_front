0b31ee1aba054e77ef3fa17458f72a88b4f9e118
PedroFlorenzano <pedroflorenzano.dev@gmail.com>
2022-11-07 22:26:46 -0300

    Melhorando a visibilidade da tela de login

--- front-end/src/app/modules/material/material.module.ts
+++ front-end/src/app/modules/material/material.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 const MaterialComponents = [
   MatButtonModule,
   MatInputModule,
-  MatIconModule
+  MatIconModule,
+  MatTooltipModule
 ]
 
 @NgModule({
